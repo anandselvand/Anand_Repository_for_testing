436c8db4b2a3faa3b2b954cc0c983cf844fb7a48
Test User <test@example.com>
2025-08-13 09:50:35 +0000

    Updated multiple files

---
+++ devops/Sample_Java_sourcescript/README.md
@@ -0,0 +1,8 @@
+# Hello Git Project
+
+This is a simple Python project for practicing Git commands.
+
+## How to run
+```bash
+python app.py
+```
---
+++ devops/Sample_Java_sourcescript/app.py
@@ -0,0 +1,7 @@
+# app.py
+def greet(name):
+    return f"Hello, {name}! Welcome to Git testing."
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    print(greet(user_name))
